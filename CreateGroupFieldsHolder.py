a8c152e1b7ddea09139c5618526cae9ec2f560c5
maria <fffreefall@gmail.com>
2025-02-11 21:45:31 +0300

    CreateGroupFieldsHolder

---
+++ CreateGroupFieldsHolder.py
@@ -0,0 +1,8 @@
+class CreateGroupFieldsHolder:
+    def __init__(self, new, group_name, group_header, group_footer, submit):
+        self.new = new
+        self.group_name = group_name
+        self.group_header = group_header
+        self.group_footer = group_footer
+        self.submit = submit
+    
