bf8e4ed8e60603c04e39a19f0a72669db83b8821
maria <fffreefall@gmail.com>
2025-12-27 15:09:28 +0300

    software_testing new modules

---
+++ software_testing/classes.py
@@ -0,0 +1,4 @@
+from datetime import date
+date_in_past = date(2000, 1, 4)
+date_in_past.weekday()
+print(date_in_past.weekday())
---
+++ software_testing/geometry.py
@@ -0,0 +1,5 @@
+from geom2d.point import Point
+
+a = Point(0,0)
+b = Point(3,4)
+print (a.distance(b))
